8734dafeeb071280d2e3ea33d8295725dcf435cc
Bharg Sharma <bharg.sharma@rea-group.com>
2018-06-20 09:48:30 +1000

    Add xcode project files

---
+++ walkfinder/walkfinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  walkfinder
+//
+//  Created by Bharg Sharma on 20/6/18.
+//  Copyright © 2018 Bharg Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
