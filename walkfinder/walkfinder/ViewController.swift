9786cb319a730e5f924b380f46bb90d64aa3dec7
Bharg Sharma <bharg.sharma@rea-group.com>
2018-07-11 14:37:51 +1000

    Display google maps and request location from user

--- walkfinder/walkfinder/ViewController.swift
+++ walkfinder/walkfinder/ViewController.swift
@@ -9,39 +9,39 @@
 import UIKit
 import GoogleMaps
 import CoreLocation
-import MapKit
 
 class ViewController: UIViewController {
+  
+  private var mapView: GMSMapView!
+  
+  let locationManager: CLLocationManager = CLLocationManager()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    mapView = GMSMapView(frame: view.frame)
+    mapView.isMyLocationEnabled = true
+    view.addSubview(mapView)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
     
-    @IBOutlet weak var mapView: MKMapView!
-
-    let authoriseCurrentLocation: CLLocationManager = CLLocationManager()
-    //class intance declaration because instantiating it inside the viewDidLoad method meant instance was released by ARC after method was had been executed
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        authoriseCurrentLocation.requestAlwaysAuthorization()
-        //authoriseCurrentLocation.requestWhenInUseAuthorization()
-        
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-//
-//        // Creates a marker in the center of the map.
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//        marker.title = "Sydney"
-//        marker.snippet = "Australia"
-//        marker.map = mapView
+    if !(CLLocationManager.authorizationStatus() == .authorizedWhenInUse) {
+      locationManager.requestWhenInUseAuthorization()
+    } else {
+      setupMapCamera()
     }
+  }
+  
+  func setupMapCamera() {
+    
+  }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+extension ViewController: CLLocationManagerDelegate {
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    setupMapCamera()
+  }
 }
 
